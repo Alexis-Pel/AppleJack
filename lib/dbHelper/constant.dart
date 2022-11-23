cfdd50638140af031f5fb60b2a7c0bbbf9c431cd
fayssal <fayssalmechmeche.pro@gmail.com>
2022-11-23 15:46:41 +0100

    PartyModel

--- lib/dbHelper/constant.dart
+++ lib/dbHelper/constant.dart
@@ -3,3 +3,4 @@ const MONGO_URL = "mongodb+srv://coding:coding@atlascluster.nmc6qpn.mongodb.net/
 const USERS_COLLECTION = "Users";
 const COURSE_COLLECTION = "Courses";
 const HORSE_COLLECTION = "Horses";
+const PARTY_COLLECTION = "Partys";
