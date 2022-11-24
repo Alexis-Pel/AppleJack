b5ac5fde0b7dde5904229f89faab567d3c6d1960
angel_rafael <generalpompeyo@hotmail.com>
2022-11-24 10:36:36 +0100

    code format improvement

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,11 +6,10 @@ import 'package:project/screens/soiree_page.dart';
 import '../dbHelper/mongoDB.dart';
 import 'package:project/screens/dashboard.dart';
 
-
 void main() async {
   //WidgetsFlutterBinding.ensureInitialized();
   await MongoDatabase.connect();
-  runApp( const Home());
+  runApp(const Home());
 }
 
 class Home extends StatelessWidget {
@@ -31,33 +30,54 @@ class Home extends StatelessWidget {
           appBar: AppBar(
             bottom: const TabBar(
               tabs: [
-
-                //Home
+                // Home
                 Tab(
-                  icon: Icon(Icons.home),
-                    child:Text(style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),"Actualité", maxLines: 1,)
-                ),
+                    icon: Icon(Icons.home),
+                    child: Text(
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                      "Actualité",
+                      maxLines: 1,
+                    )),
 
                 //Parties
                 Tab(
-                  icon: Icon(Icons.local_fire_department),
-                    child:Text(style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),"Soirée", maxLines: 1,)
-                ),
+                    icon: Icon(Icons.local_fire_department),
+                    child: Text(
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                      "Soirée",
+                      maxLines: 1,
+                    )),
 
                 //Concours
                 Tab(
-                  icon: Icon(Icons.sports_score),
-                    child:Text(style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),"Concours", maxLines: 1,)
-                ),
+                    icon: Icon(Icons.sports_score),
+                    child: Text(
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                      "Concours",
+                      maxLines: 1,
+                    )),
 
                 //Courses
-                Tab(icon: Icon(Icons.calendar_month_outlined),
-                  child:Text(style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),"Cours", maxLines: 1,),),
+                Tab(
+                  icon: Icon(Icons.calendar_month_outlined),
+                  child: Text(
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                    "Cours",
+                    maxLines: 1,
+                  ),
+                ),
 
                 //Profile
                 Tab(
                   icon: Icon(Icons.account_circle),
-                  child:Text(style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),"Profil", maxLines: 1,),
+                  child: Text(
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                    "Profil",
+                    maxLines: 1,
+                  ),
                 ),
               ],
             ),
@@ -77,4 +97,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
